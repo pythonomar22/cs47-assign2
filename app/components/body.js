c09083743f023a4f4bdb8c637a12c502b6cc9a7f
Omar Abul-Hassan <omarabulhassan@gmail.com>
2023-02-03 22:26:16 -0800

    second

--- app/components/body.js
+++ app/components/body.js
@@ -3,6 +3,8 @@ import {View, Text, StyleSheet, Image, ImageBackground} from 'react-native'
 import Profiles from "../../assets/Profiles";
 import Themes from "../../assets/Themes";
 import {useState} from "react";
+import Icons from "../../assets/Icons";
+import SwipeCards from 'react-native-swipe-cards';
 
 const Body = () => {
     console.log('Profiles', Profiles.mtl);
@@ -14,7 +16,9 @@ const Body = () => {
                 <Text style={styles.text} >{Profiles.mtl.caption}</Text>
             </ImageBackground>
             <View style={styles.rectangle}>
-                <Text style={styles.rectangleText}>My Hottest Take</Text>
+                <Text style={styles.rectangleText}>My hottest take</Text>
+                <Image source={Icons.audioWave.light} style={styles.soundwave}/>
+                <Image source={Icons.player.light} style={styles.player} />
             </View>
         </View>
     );    
@@ -34,6 +38,9 @@ const styles = StyleSheet.create({
         aspectRatio: 1/1.1,
         height: undefined,
         justifyContent: "space-between",
+        //    shadowColor: shadow.shadowColor,
+   // shadowOffset: shadow.shadowOffset,
+    //shadowRadius: shadow.shadowRadius,
 
     },
     imageStyle: {
@@ -47,16 +54,42 @@ const styles = StyleSheet.create({
     },
     rectangle: {
         width: "100%",
-        height: 100,
+        height: 125,
         backgroundColor: "white",
         borderRadius: 8,
         marginTop: 20,
         shadowColor: "black",
+        // i deleted the shadow settings in themes but theyre the same as the one in "image" if they worked
         shadowOpacity: 0.4,
         shadowRadius: 8,
         shadowOffset: { width: 0, height: 2 },
         elevation: 5
-    }
+    },
+    soundwave: {
+        resizeMode: "contain",
+        width: 250,
+        height: 300,
+        position: "absolute",
+        right: 10,
+        bottom: -100,
+    },
+    player: {
+        resizeMode: "contain",
+        width: 70,
+        height: 70,
+        bottom: -40,
+        left: 10,
+    },
+    rectangleText: {
+        position: "absolute",
+        top: 0,
+        left: 0,
+        color: "black",
+        padding: 8, 
+        textAlign: "center",
+        fontSize: 20,
+        fontFamily: "Sydney"
+    },
 });
 
 export default Body;
